064cb82fa7a6a5f8d43fb41d984b8d5f03811f10
Jordan Sleeper <jordanpsleeper@gmail.com>
2021-03-15 22:55:16 -0400

    Types

--- src/helpers/aliasPaths.ts
+++ src/helpers/aliasPaths.ts
@@ -3,8 +3,8 @@ const tsconfig = require('../../tsconfig.json');
 
 // transforms
 // "~components/*": ["./src/components/*"] => `~components/: path.resolve("./src/components")`
-const aliases = Object.entries(tsconfig.compilerOptions.paths).reduce((acc, value) => {
-  const [key, pathValue] = value.flat().map((i) => i.replace('/*', ''));
+const aliases = Object.entries(tsconfig.compilerOptions.paths).reduce((acc, value: any[]) => {
+  const [key, pathValue] = value.flat().map((i: string) => i.replace('/*', ''));
   acc[key] = path.resolve(pathValue);
   return acc;
 }, {});
